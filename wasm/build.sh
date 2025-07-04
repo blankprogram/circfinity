ab0dee06a577e5613a26324a7a1afbcea9288f79
blankprogram <blankfragz@gmail.com>
2025-07-05 02:21:15 +1000

    not override toolchain

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+
 rm -rf build && mkdir build && cd build
+
+EXTRA_FLAGS=""
+if [[ -z "${CI:-}" ]]; then
+    EXTRA_FLAGS="-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$HOME/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
+fi
 
 emcmake cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake \
     -DVCPKG_MANIFEST_MODE=ON \
     -DVCPKG_TARGET_TRIPLET=wasm32-emscripten \
-    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="$HOME/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" \
+    $EXTRA_FLAGS \
     ..
 
 cmake --build .
