bd5a803331a75a011b1fec4f0cb6591f6e857343
blankprogram <blankfragz@gmail.com>
2025-07-05 14:25:05 +1000

    cleaned up and caching build

--- wasm/build.sh
+++ wasm/build.sh
@@ -3,12 +3,11 @@ set -euxo pipefail
 
 rm -rf build && mkdir build && cd build
 
-# Correctly combine vcpkg and emscripten toolchains
 emcmake cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_TESTS=OFF \
     -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake \
-    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" \
     -DVCPKG_TARGET_TRIPLET=wasm32-emscripten \
     ..
 
