f76d53bc9619e676575baf6841cfb71d7a10bfe3
blankprogram <blankfragz@gmail.com>
2025-07-05 12:04:04 +1000

    cmake fix

--- wasm/build.sh
+++ wasm/build.sh
@@ -3,8 +3,9 @@ set -euxo pipefail
 
 rm -rf build && mkdir build && cd build
 
-emcmake cmake -G Ninja \
+cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake \
     -DVCPKG_MANIFEST_MODE=ON \
     -DVCPKG_TARGET_TRIPLET=wasm32-emscripten \
     ..
