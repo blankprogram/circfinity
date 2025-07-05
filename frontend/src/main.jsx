5d42e6cb7d3a6ef68c01fad46c47df2e7f7837b0
blankprogram <blankfragz@gmail.com>
2025-07-05 13:37:17 +1000

    pelase work

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,24 @@
 // src/main.jsx
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home.jsx'
-import Expr from './Expr.jsx'
-import useWasm from './hooks/Wasm.js'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home.jsx";
+import Expr from "./Expr.jsx";
+import useWasm from "./hooks/Wasm.js";
+import "./index.css";
 
 function App() {
   // load wasm only once
-  const wasm = useWasm()
+  const wasm = useWasm();
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/circfinity">
       <Routes>
         <Route path="/" element={<Home wasm={wasm} />} />
         <Route path="/:n" element={<Expr wasm={wasm} />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById("root")).render(<App />);
--- wasm/build.sh
+++ wasm/build.sh
@@ -3,12 +3,13 @@ set -euxo pipefail
 
 rm -rf build && mkdir build && cd build
 
-cmake -G Ninja \
+# Correctly combine vcpkg and emscripten toolchains
+emcmake cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_TESTS=OFF \
     -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake \
-    -DVCPKG_MANIFEST_MODE=ON \
+    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
     -DVCPKG_TARGET_TRIPLET=wasm32-emscripten \
     ..
 
-cmake --build .
+cmake --build . --target wasm_main
